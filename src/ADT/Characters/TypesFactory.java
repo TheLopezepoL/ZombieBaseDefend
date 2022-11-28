206386054ad3e150434801cd2cbef89334da02ff
Sebastián L <thelopezepol@estudiantec.cr>
2022-11-27 20:52:14 -0600

    typesFactory

--- src/ADT/Characters/TypesFactory.java
+++ src/ADT/Characters/TypesFactory.java
@@ -4,25 +4,22 @@ import ADT.CharacterTypes.Allies.Estructura;
 import ADT.CharacterTypes.Allies.EstructuraAerea;
 import ADT.CharacterTypes.Allies.EstructuraExplosiva;
 import ADT.CharacterTypes.Allies.EstructuraOfensiva;
+import ADT.CharacterTypes.Zombies.Zombie;
+import ADT.CharacterTypes.Zombies.ZombieAereo;
+import ADT.CharacterTypes.Zombies.ZombieImpacto;
 import ADT.Enums.EnumCharacters;
 
 public class TypesFactory {
     //FACTORY METHOD
     public aTipo createType (EnumCharacters tipo){
-        switch (tipo){
-            case CONTACTO:
-            case MEDIANO:
-                return new EstructuraOfensiva();
-            case AEREO:
-                return new EstructuraAerea();
-            case IMPACTO:
-                return new EstructuraExplosiva();
-            case BLOQUE:
-                return new Estructura();
-            case MULTIPLE:
-                return new EstructuraOfensiva();
-            default:
-                throw new IllegalArgumentException("Tipo de personaje no existe");
-        }
+        return switch (tipo) {
+            case ESTRUCTURA_CONTACTO, ESTRUCTURA_MEDIO_ALCANCE, ESTRUCTURA_ATAQUE_MULTIPLE -> new EstructuraOfensiva();
+            case ESTRUCTURA_AEREO -> new EstructuraAerea();
+            case ESTRUCTURA_IMPACTO -> new EstructuraExplosiva();
+            case ESTRUCTURA_BLOQUE -> new Estructura();
+            case ZOMBIE_CONTACTO, ZOMBIE_MEDIO_ALCANCE -> new Zombie();
+            case ZOMBIE_AEREO -> new ZombieAereo();
+            case ZOMBIE_IMPACTO -> new ZombieImpacto();
+        };
     }
 }
